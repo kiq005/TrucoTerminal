20b4121bdfd4042e26e9b446ed60e8890d3cfc0b
kiq005 <kiq005@gmail.com>
2017-08-03 12:59:40 -0300

    Updates do dia 03-08-17

--- src/c-poo/oo.c
+++ src/c-poo/oo.c
@@ -4,54 +4,82 @@
 function(public, getName,
 	//printf("Type: %s\n", (char*)this.attributes[0]->_type);
 	//printf("Nome: %s\n", (char*)this.attributes[0]->_value);
-	return get(nome);
+	return "Fulano";
 	);
 function(public, setName, 
 	char* name = va_arg(args, char*);
 	set(nome, name);
 	);
 function(public, setIdade, 
-	int idade = va_arg(args, int);
-	set(idade, (generic)(intptr_t)idade);
+	int i = va_arg(args, int);
+	set(idade, (generic)(intptr_t)i);
 	);
 function(public, getIdade,
 	return get(idade);
+	);
+function(public, setAnoFormatura, 
+	int i = va_arg(args, int);
+	set(anoFormatura, (generic)(intptr_t)i);
+	);
+function(public, getAnoFormatura, 
+	return get(anoFormatura);
 	);
 
 /* Definição de atributos */
 var(public, char[64], nome);
 var(public, int, idade);
+var(public, int, anoFormatura);
 /* Definição das classes */
 class(Pessoa, {&getName, &setName}, {&nome});
 extends(Aluno, Pessoa, {&setIdade, &getIdade}, {&idade});
 extends(Professor, Pessoa);
+extends(Formado, Aluno, {&getAnoFormatura, &setAnoFormatura}, {&anoFormatura});
+/* Definição de sobrecargas */
+override(Aluno, getName, 
+	return get(nome);
+	);
 
 int main(void){
 	// Instaciações
 	puts(">> Criando instâncias");
-	instanciate(fulano, Pessoa);
-	instanciate(aluno1, Aluno);
-	instanciate(aluno2, Aluno);
-	instanciate(aluno3, Aluno);
-	instanciate(prof, Professor);
+	//puts(">>>> fulano");
+	instanciate(Pessoa, fulano);
+	//puts(">>>> aluno1");
+	instanciate(Aluno, aluno1);
+	//puts(">>>> aluno2");
+	instanciate(Aluno, aluno2);
+	//puts(">>>> aluno3");
+	instanciate(Aluno, aluno3);
+	//puts(">>>> prof");
+	instanciate(Professor, prof);
+	//puts(">>>> form");
+	instanciate(Formado, form);
 	// Teste de Chamada de funções
-	//puts(">> Chamando funções em fulano");
-	//printf("Nome: %s\n", (char*)call(fulano, getName));
-	//call(fulano, setName, "João");
-	//call(fulano, setIdade, 32);
-	//printf("Nome: %s\n", (char*)call(fulano, getName));
-	//printf("Idade: %d\n", (int)call(fulano, getIdade));
+	puts(">> Chamando funções em fulano");
+	printf("Nome: %s\n", (char*)call(fulano, getName));
+	call(fulano, setName, "João");
+	call(fulano, setIdade, 32);
+	printf("Nome: %s\n", (char*)call(fulano, getName));// a chamada getName retorna "Fulano"
+	printf("Idade: %d\n", (int)(intptr_t)call(fulano, getIdade));
 	call(fulano, setName, "Josevaldo");
 	call(fulano, setIdade, 34);
 	printf("Nome: %s\n", (char*)call(fulano, getName));
 	printf("Idade: %d\n", (int)(intptr_t)call(fulano, getIdade));
 
+	/* Testando herança direta */
+	puts(">> Chamando funções em aluno1");
 	call(aluno1, setName, "Ana");
 	call(aluno1, setIdade, 18);
-	printf("Nome: %s\n", (char*)call(aluno1, getName));
+	printf("Nome: %s\n", (char*)call(aluno1, getName));// A chamada getName vai para o método sobrecarregado
 	printf("Idade: %d\n", (int)(intptr_t)call(aluno1, getIdade));
-/*
-	puts("---");
+	/* Testando herança em profundidade */
+	puts(">> Chamando funções em form");
+	call(form, setName, "Carlos");
+	call(form, setIdade, 26);
+	printf("Nome: %s\n", (char*)call(form, getName));// A chamada getName vai para o método sobrecarregado em Aluno
+	printf("Idade: %d\n", (int)(intptr_t)call(form, getIdade));
+	
+	/* Testando várias instancias */
 	puts(">> Definindo alunos");
 	call(aluno1, setName, "Ana");
 	call(aluno2, setName, "Bia");
@@ -62,13 +90,13 @@ int main(void){
 	puts(">> Obtendo nomes dos alunos");
 	
 	printf("Nome: %s\n", (char*)call(aluno1, getName));
-	printf("Idade: %d\n", (int)call(aluno1, getIdade));
+	printf("Idade: %d\n", (int)(intptr_t)call(aluno1, getIdade));
 	printf("Nome: %s\n", (char*)call(aluno2, getName));
-	printf("Idade: %d\n", (int)call(aluno2, getIdade));
+	printf("Idade: %d\n", (int)(intptr_t)call(aluno2, getIdade));
 	printf("Nome: %s\n", (char*)call(aluno3, getName));
-	printf("Idade: %d\n", (int)call(aluno3, getIdade));
+	printf("Idade: %d\n", (int)(intptr_t)call(aluno3, getIdade));
 	printf("Nome: %s\n", (char*)call(fulano, getName));
-	printf("Idade: %d\n", (int)call(fulano, getIdade));
+	printf("Idade: %d\n", (int)(intptr_t)call(fulano, getIdade));
 
 	puts(">> Redefinindo nomes dos alunos");
 	call(aluno1, setName, "Ana Lee");
@@ -78,7 +106,14 @@ int main(void){
 	printf("Nome: %s\n", (char*)call(aluno2, getName));
 	printf("Nome: %s\n", (char*)call(aluno3, getName));
 	printf("Nome: %s\n", (char*)call(fulano, getName));
-*/
+
+	/* Teste de desalocação */
+	puts(">> Desalocando aluno1");
+	delete(aluno1);
+	call(aluno1, setName, "Ana");
+	call(aluno1, setIdade, 18);
+	printf("Nome: %s\n", (char*)call(aluno1, getName));
+	printf("Idade: %d\n", (int)(intptr_t)call(aluno1, getIdade));
 
 	return 1;
 }
--- src/c-poo/oo.h
+++ src/c-poo/oo.h
@@ -18,14 +18,17 @@ int __get_attribute_index__(Object this, char* att_name);
 generic __realize_call__(Object _obj_, char* func, ...);
 generic __find_method_on_object__(Object _obj_, char* func, va_list args);
 
-static int __i__, __j__;
+static int __i__, __j__, __methods__=0;
 static Object _scope_;
+static Class _super_;
 
 #define class(name, ...) Class name = {NULL, #name, __VA_ARGS__}
 #define extends(name, super, ...) Class name = {&super, #name, __VA_ARGS__}
-#define instanciate(name, _type_) \
+/*Precisamos trocar o modo de definição de atributos para uma variavel em loop while(!=null)
+ assim conseguiremos herdar corretamente de classes avós!!!*/
+#define instanciate(_type_, name) \
 	Object* name = (Object*)malloc(sizeof(Object));\
-	name->type = _type_;\
+	name->_type = &_type_;\
 	for(__i__=0;__i__<MAX_MET; __i__++){\
 		name->attributes[__i__]=(Attribute*)malloc(sizeof(Attribute));\
 		if(_type_._attributes[__i__]==NULL){\
@@ -37,24 +40,59 @@ static Object _scope_;
 		strncpy(name->attributes[__i__]->_name, _type_._attributes[__i__]->_name, MAX_LEN);\
 		name->attributes[__i__]->_value = NULL;\
 	}\
-	for(__j__=0;__i__<MAX_MET; __i__++, __j__++){\
+	/*Herdando atributos de classes superiores*/\
+	if(name->_type!=NULL){\
+		_super_ = *name->_type;\
+		while(1){\
+			/*printf("DEBUG: vendo atributos de %s.\n", _super_._name);*/\
+			for(__j__=0;__i__<MAX_MET && _super_._attributes[__j__]!=NULL; __i__++, __j__++){\
+				name->attributes[__i__]=(Attribute*)malloc(sizeof(Attribute));\
+				if(_super_._attributes[__j__]==NULL) continue; \
+				name->attributes[__i__]->_visibility = _super_._attributes[__j__]->_visibility;\
+				strncpy(name->attributes[__i__]->_type, _super_._attributes[__j__]->_type, MAX_LEN);\
+				strncpy(name->attributes[__i__]->_name, _super_._attributes[__j__]->_name, MAX_LEN);\
+				name->attributes[__i__]->_value = NULL;\
+				/*printf("DEBUG: Adicionado %s á %s.\n", name->attributes[__i__]->_name, #name);*/\
+			}\
+			if(_super_._super==NULL) break;\
+			_super_ = *_super_._super;\
+		}\
+	}\
+	/*Finaliza instanciação*/\
+	for(;__i__<MAX_MET;__i__++){\
 		name->attributes[__i__]=(Attribute*)malloc(sizeof(Attribute));\
-		if(_type_._super->_attributes[__j__]==NULL) continue; \
-		name->attributes[__i__]->_visibility = _type_._super->_attributes[__j__]->_visibility;\
-		strncpy(name->attributes[__i__]->_type, _type_._super->_attributes[__j__]->_type, MAX_LEN);\
-		strncpy(name->attributes[__i__]->_name, _type_._super->_attributes[__j__]->_name, MAX_LEN);\
-		name->attributes[__i__]->_value = NULL;\
 	}
+
+
 #define function(visibility, name, cmd) \
 	generic _##name (Object this, va_list args){cmd}; Method name = {visibility, #name, &_##name}
+#define _override_function_(visibility, _name_, nameidx, _class_, cmd) \
+	generic _##_name_##nameidx (Object this, va_list args){cmd}; Method _o_##_name_##nameidx= {public, #_name_, &_##_name_##nameidx};\
+	/*GNU C*/\
+	__attribute__((constructor)) _init_##_name_##nameidx(){\
+		_o_##_name_##nameidx._visibility = _name_._visibility;\
+		for(__i__=0; __i__<MAX_MET; __i__++){ \
+			if(_class_.methods[__i__]==NULL){ \
+				_class_.methods[__i__]=&_o_##_name_##nameidx; \
+				break; \
+			} \
+		} \
+	};
+#define override(_class_, name, cmd) \
+	_override_function_(name._visibility, name, __methods__, _class_, cmd)
 #define var(visibility, type, name) \
 	Attribute name = {visibility, #type, #name};
 #define get(att_name)\
 	this.attributes[__get_attribute_index__(this, #att_name)]->_value;
 #define set(att_name, __value__)\
+	/*printf("DEBUG: Estamos na classe %s.\n", this._type->_name);*/\
 	this.attributes[__get_attribute_index__(this, #att_name)]->_value = __value__;
-
-
+	
+#define delete(_obj_)\
+	for(__i__=0; __i__<MAX_MET; __i__++){\
+		free(_obj_->attributes[__i__]);\
+	}\
+	free(_obj_)
 
 struct Class{
 	Class *_super;
@@ -77,66 +115,38 @@ struct Method{
 };
 
 struct Object{
-	Class type;
+	Class *_type;
 	Attribute *attributes[MAX_MET];
 };
 
 int __get_attribute_index__(Object this, char* att_name){
+	/*fprintf(stderr, "DEBUG: Procurando em %s o atributo %s.\n", this._type->_name, att_name);*/
 	for(__i__=0; __i__<MAX_MET; __i__++){
 		if(strcmp(att_name, (*this.attributes[__i__])._name)==0) return __i__;
 	}
-	fprintf(stderr, "Erro: A classe '%s' não possui um atributo '%s'.\n", this.type._name, att_name);
+	fprintf(stderr, "Erro: A classe '%s' não possui um atributo '%s'.\n", this._type->_name, att_name);
 	return -1;
-}
-
-/* Esta função não é mais utilizada, pois não permite passagem de argumentos
- para uma super classe. Esta aqui somente para consultas, e logo deverá ser
- apagada.
-*/
-generic __find_method_on_class__(Class _class_, char* func, ...){
-	for(__i__=0;_class_.methods[__i__]!=NULL && __i__<MAX_MET;__i__++){
-		if(strcmp(_class_.methods[__i__]->_name, func)==0){
-			va_list args;
-			generic _ret;
-			if(_class_.methods[__i__]->_visibility==private){
-				if(strcmp(_class_._name, _scope_.type._name)==0){
-					_class_.methods[__i__]->_function();
-					return NULL;
-				}
-				fprintf(stderr, "Erro: A função '%s' é privada em '%s'.\n", func, _class_._name);
-				return NULL;
-			}
-			va_start(args, func);
-			_ret = (generic) _class_.methods[__i__]->_function(args);
-			va_end(args);
-			return _ret;
-		}
-	}// Se não encontra o método na classe filha, procura na super
-	if(_class_._super!=NULL){
-		return __find_method_on_class__(*_class_._super, func);
-	}
-	fprintf(stderr, "Erro: Não é possível chamar a função '%s' em '%s'.\n", func, _class_._name);
-	return NULL;
 }
 
 generic __find_method_on_object__(Object _obj_, char* func, va_list args){
 	generic _ret;
-	for(__i__=0;_obj_.type.methods[__i__]!=NULL && __i__<MAX_MET;__i__++){
-		if(strcmp(_obj_.type.methods[__i__]->_name, func)==0){
-			if(_obj_.type.methods[__i__]->_visibility==private){
-				if(!strcmp(_obj_.type._name, _scope_.type._name)==0){
-					fprintf(stderr, "Erro: A função '%s' é privada em '%s'.\n", func, _obj_.type._name);
+	for(__i__=0;_obj_._type->methods[__i__]!=NULL && __i__<MAX_MET;__i__++){
+		if(strcmp(_obj_._type->methods[__i__]->_name, func)==0){
+			if(_obj_._type->methods[__i__]->_visibility==private){
+				if(!strcmp(_obj_._type->_name, _scope_._type->_name)==0){
+					fprintf(stderr, "Erro: A função '%s' é privada em '%s'.\n", func, _obj_._type->_name);
 					return NULL;
 				}
 			}
-			_ret = (generic)_obj_.type.methods[__i__]->_function(_obj_, args);
+			_ret = (generic)_obj_._type->methods[__i__]->_function(_obj_, args);
 			return _ret;
 		}
 	}// Se não encontra o método na classe filha, procura na super
-	if(_obj_.type._super!=NULL){
+	/*printf("Procurando na super de %s\n", _obj_._type->_name);*/
+	if(_obj_._type->_super!=NULL){
 		// Criamos uma instância da super
 		Object* super = (Object*)malloc(sizeof(Object));
-		super->type = *_obj_.type._super;
+		super->_type = _obj_._type->_super;
 		for(__i__=0;__i__<MAX_MET; __i__++){// Alocamos seus atributos
 			super->attributes[__i__]=(Attribute*)malloc(sizeof(Attribute));
 			//if((*_obj_.type._super)._attributes[__i__]==NULL) continue;
@@ -159,7 +169,7 @@ generic __find_method_on_object__(Object _obj_, char* func, va_list args){
 		return _ret;
 		//return (generic) __find_method_on_class__(*_obj_.type._super, func);
 	}
-	fprintf(stderr, "Erro: A classe '%s' não possui a função '%s'.\n", _obj_.type._name, func);
+	fprintf(stderr, "Erro: A classe '%s' não possui a função '%s'.\n", _obj_._type->_name, func);
 	return NULL;
 }
 
